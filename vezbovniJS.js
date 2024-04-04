2317f01d559bd8e6ee2d2ec96f80f72322ad836a
dragisamitpredavanja <dragisa.mitic@cetrnaestgim.edu.rs>
2024-04-04 15:42:22 +0200

    vezbovnijs

---
+++ vezbovniJS.js
@@ -0,0 +1,12 @@
+function oboji() {
+    sifraBoje=document.getElementById('boja').value;
+    document.getElementById('kut2').style.backgroundColor=sifraBoje
+    
+}
+function saberi() {
+   let broj1=Number(document.getElementById('br1').value);
+   let broj2=Number(document.getElementById('br2').value);
+   console.log(typeof broj1)
+   let zbir=broj1+broj2
+   alert('zbir='+zbir)
+}
