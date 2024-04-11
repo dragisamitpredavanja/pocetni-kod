bb8cb3bb7c422b4402ceb2060a82bae72358fe7e
dragisamitpredavanja <dragisa.mitic@cetrnaestgim.edu.rs>
2024-04-11 08:59:01 +0200

    temperaturaFC i CF

--- funkcije.js
+++ funkcije.js
@@ -1,7 +1,11 @@
 let a=5
 let b=7
+const c=document.getElementById('br1').value;
+console.log(c)
 function zbir1(x,y) {
+   
     let zbir=x+y
+    console.log(c)
     return zbir
     
 }
@@ -10,4 +14,19 @@ function zbir2(x,y) {
 }
 const zbir3=(x,y)=>{
     return x+y
+}
+function oboji() {
+    let sifraBoje=document.getElementById('boja').value;
+    document.getElementById('kon2').style.backgroundColor=sifraBoje
+    
+}
+function celzijusToFarenhajd(tc) {
+    let tf=tc*1.8+32
+    document.getElementById('far').innerHTML=tf+' F'
+    
+}
+function farenfajdToCelzijus(tf) {
+    let tc=(tf-32)/1.8
+    document.getElementById('cel2').innerHTML=tc+' C'
+    
 }
